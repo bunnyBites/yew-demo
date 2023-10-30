5610d8e81f38b3e4e66dcf8e5e3dde4f6b612793
Bunny Bites <s.mukeshkr481@gmail.com>
2023-10-30 20:17:36 +0530

    add hello-world app in yew

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use yew::prelude::*;
+
+#[function_component]
+// the name of this function is passed to the renderer struct
+fn AppRoot() -> Html {
+    html!(
+        <div>{"hello world!"}</div>
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<AppRoot>::new().render();
 }

c4719f969e21e75e3052abbaf80aff2e0331ed85
Bunny Bites <s.mukeshkr481@gmail.com>
2023-10-31 09:57:52 +0530

    add appbar

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,23 @@ mod pages;
 // the name of this function is passed to the renderer struct
 fn AppRoot() -> Html {
     html!(
-        <div class="border border-primary container-fluid">
-            <div class="row min-vh-100 align-items-center justify-content-center">
-                <div class="col-sm-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <pages::login::Login />
+        <>
+            // appbar section
+            <components::appbar::Appbar />
+
+            // Login section
+            <div class="container-fluid">
+                <div class="row min-vh-100 align-items-center justify-content-center">
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <pages::login::Login />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/pages/login.rs
+++ src/pages/login.rs
@@ -7,7 +7,7 @@ pub fn Login() -> Html {
         <form>
             <components::input::Input label="Username" field_type="text" name="username" />
             <components::input::Input label="Password" field_type="password" name="password" />
-            <button type="submit" class="btn btn-primary w-100">{"Submit"}</button>
+            <button type="submit" class="btn btn-info w-100">{"Login"}</button>
         </form>
     }
 }

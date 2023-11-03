6a8803838356e23ffdd3729207f9be333d5a0ec5
Bunny Bites <s.mukeshkr481@gmail.com>
2023-11-03 09:31:49 +0530

    add login functionality to hold and use state

--- src/components/input.rs
+++ src/components/input.rs
@@ -5,17 +5,24 @@ pub struct InputProps {
     pub label: AttrValue,
     pub name: AttrValue,
     pub field_type: AttrValue,
+    pub onchange: Callback<Event>,
+    pub value: String,
 }
 
 #[function_component]
 pub fn Input(props: &InputProps) -> Html {
+    let field_id = format!("field-{}", props.name);
+
     html! {
         <div class="mb-3">
-            <label class="form-label">{props.label.clone()}</label>
+            <label for={field_id.clone()} class="form-label">{props.label.clone()}</label>
             <input
+                id={field_id}
                 type={props.field_type.clone()}
                 class="form-control"
                 name={props.name.clone()}
+                value={props.value.clone()}
+                onchange={props.onchange.clone()}
             />
         </div>
     }
--- src/pages/login.rs
+++ src/pages/login.rs
@@ -1,12 +1,63 @@
-use crate::components;
+use gloo_console::log;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
+
+use crate::components::{self};
 
 #[function_component]
 pub fn Login() -> Html {
+    let username_handle = use_state(String::default);
+    let username = (*username_handle).clone();
+
+    let update_username = Callback::from(move |e: Event| {
+        e.prevent_default();
+        let input_element = e.target_dyn_into::<HtmlInputElement>();
+
+        if let Some(input) = input_element {
+            username_handle.set(input.value());
+        }
+    });
+
+    let password_handle = use_state(String::default);
+    let password = (*password_handle).clone();
+
+    let update_password = Callback::from(move |e: Event| {
+        let input_element = e.target_dyn_into::<HtmlInputElement>();
+
+        if let Some(input) = input_element {
+            password_handle.set(input.value());
+        }
+    });
+
+    let cloned_username = username.clone();
+    let cloned_password = password.clone();
+
+    let submit_form = Callback::from(move |e: SubmitEvent| {
+        e.prevent_default();
+
+        log!(
+            "user details",
+            cloned_username.clone(),
+            cloned_password.clone()
+        );
+    });
+
     html! {
-        <form>
-            <components::input::Input label="Username" field_type="text" name="username" />
-            <components::input::Input label="Password" field_type="password" name="password" />
+        <form onsubmit={submit_form}>
+            <components::input::Input
+                label="Username"
+                field_type="text"
+                name="username"
+                onchange={update_username}
+                value={username}
+            />
+            <components::input::Input
+                label="Password"
+                field_type="password"
+                name="password"
+                onchange={update_password}
+                value={password}
+            />
             <button type="submit" class="btn btn-info w-100">{"Login"}</button>
         </form>
     }
